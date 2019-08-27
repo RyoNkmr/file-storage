1d98af6f781add8f42378b5fb2d5f8f6ff962c23
RyoNkmr <ryonakamuraryo@gmail.com>
2019-08-27 16:20:49 +0900

    add comments

--- storage.go
+++ storage.go
@@ -1,19 +1,18 @@
-package storage
+package filestorage
 
 import (
 	"errors"
-	"fmt"
-	"io/ioutil"
-	"os"
 	"path"
-	"reflect"
-	"sync"
 	"time"
-
-	"github.com/vmihailenco/msgpack"
 )
 
-type FileStorage interface {
+var (
+	ErrNoData      = errors.New("fileStorage: no data")           // file accessing methods returns this error when the given key has no data
+	ErrNeverExpire = errors.New("filetray: expiredAt is not set") // IsExpired method on fileStorage returns this error when expiredAt is nil for the key
+)
+
+// A storage with cache implementation
+type Storage interface {
 	Get(key string, dest interface{}) error
 	NoCacheGet(key string, dest interface{}) error
 	IsExpired(key string) (isExpired bool, err error)
@@ -32,8 +31,6 @@ func NewFileStorage(dirpath string) *fileStorage {
 		trays:   map[string]*tray{},
 	}
 }
-
-var ErrNoData = errors.New("fileStorage: no data")
 
 func (f *fileStorage) get(key string, dest interface{}, useCache bool) (err error) {
 	if _, ok := f.trays[key]; !ok {
@@ -69,101 +66,4 @@ func (f *fileStorage) Set(key string, value interface{}, expiredAt *time.Time) e
 		f.trays[key] = newTray(f.dirpath, key, expiredAt)
 	}
 	return f.trays[key].set(value)
-}
-
-type tray struct {
-	path      string
-	cache     []byte
-	mut       sync.RWMutex
-	expiredAt *time.Time
-}
-
-var ErrNeverExpire = errors.New("filetray: expiredAt is not set")
-
-func newTray(dirpath, filename string, expiredAt *time.Time) *tray {
-	path := path.Join(dirpath, filename)
-	return &tray{
-		path:      path,
-		mut:       sync.RWMutex{},
-		expiredAt: expiredAt,
-	}
-}
-
-func (s *tray) isExpired() (isExpired bool, err error) {
-	if s.expiredAt == nil {
-		return false, ErrNeverExpire
-	}
-	return s.expiredAt.Before(time.Now()), nil
-}
-
-func (s *tray) clear() (err error) {
-	s.mut.Lock()
-	defer s.mut.Unlock()
-
-	s.cache = nil
-	if _, err = os.Stat(s.path); err == nil {
-		fmt.Println("file exist, removed")
-		return os.Remove(s.path)
-	}
-	return err
-}
-
-func (s *tray) get(dest interface{}, useCache bool) (err error) {
-	rv := reflect.ValueOf(dest)
-	if rv.Kind() != reflect.Ptr {
-		return errors.New("must pass a pointer")
-	}
-
-	if isExpired, _ := s.isExpired(); isExpired {
-		if err = s.clear(); err != nil {
-			return err
-		}
-		fmt.Println("expired")
-		return ErrNoData
-	}
-
-	if useCache && s.cache != nil {
-		err = msgpack.Unmarshal(s.cache, dest)
-		return err
-	}
-
-	s.mut.RLock()
-	defer s.mut.RUnlock()
-
-	if _, err = os.Stat(s.path); err != nil {
-		return ErrNoData
-	}
-
-	file, err := os.Open(s.path)
-	if err != nil {
-		return err
-	}
-
-	defer file.Close()
-	b, err := ioutil.ReadAll(file)
-	if err != nil {
-		return err
-	}
-
-	err = msgpack.Unmarshal(b, dest)
-	return err
-}
-
-func (s *tray) set(value interface{}) (err error) {
-	s.mut.Lock()
-	defer s.mut.Unlock()
-	file, err := os.OpenFile(s.path, os.O_WRONLY|os.O_CREATE, 0666)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	var b []byte
-	if b, err = msgpack.Marshal(value); err != nil {
-		return err
-	}
-	if _, err := file.Write(b); err != nil {
-		return err
-	}
-	s.cache = b
-	return nil
 }
---
+++ tray.go
@@ -0,0 +1,109 @@
+package filestorage
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"reflect"
+	"sync"
+	"time"
+
+	"github.com/vmihailenco/msgpack"
+)
+
+type tray struct {
+	path      string
+	cache     []byte
+	mut       sync.RWMutex
+	expiredAt *time.Time
+}
+
+func newTray(dirpath, filename string, expiredAt *time.Time) *tray {
+	path := path.Join(dirpath, filename)
+	return &tray{
+		path:      path,
+		mut:       sync.RWMutex{},
+		expiredAt: expiredAt,
+	}
+}
+
+func (s *tray) isExpired() (isExpired bool, err error) {
+	if s.expiredAt == nil {
+		return false, ErrNeverExpire
+	}
+	return s.expiredAt.Before(time.Now()), nil
+}
+
+func (s *tray) clear() (err error) {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+
+	s.cache = nil
+	if _, err = os.Stat(s.path); err == nil {
+		fmt.Println("file exist, removed")
+		return os.Remove(s.path)
+	}
+	return err
+}
+
+func (s *tray) get(dest interface{}, useCache bool) (err error) {
+	rv := reflect.ValueOf(dest)
+	if rv.Kind() != reflect.Ptr {
+		return errors.New("must pass a pointer")
+	}
+
+	if isExpired, _ := s.isExpired(); isExpired {
+		if err = s.clear(); err != nil {
+			return err
+		}
+		fmt.Println("expired")
+		return ErrNoData
+	}
+
+	if useCache && s.cache != nil {
+		err = msgpack.Unmarshal(s.cache, dest)
+		return err
+	}
+
+	s.mut.RLock()
+	defer s.mut.RUnlock()
+
+	if _, err = os.Stat(s.path); err != nil {
+		return ErrNoData
+	}
+
+	file, err := os.Open(s.path)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+	b, err := ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+
+	err = msgpack.Unmarshal(b, dest)
+	return err
+}
+
+func (s *tray) set(value interface{}) (err error) {
+	s.mut.Lock()
+	defer s.mut.Unlock()
+	file, err := os.OpenFile(s.path, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	var b []byte
+	if b, err = msgpack.Marshal(value); err != nil {
+		return err
+	}
+	if _, err := file.Write(b); err != nil {
+		return err
+	}
+	s.cache = b
+	return nil
+}
